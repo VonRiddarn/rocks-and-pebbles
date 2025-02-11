b5d6c14180802563666a17dd4656c6a18e7f2f6f
VonRiddarn <vonriddarn@gmail.com>
2025-02-11 22:56:15 +0100

    feat: added hrefs for socials AND have them open in new tab with legacy browser protection

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -3,8 +3,8 @@ import "./Socials.scss";
 const Socials = ({iconSize = "48px", direction = "row"}: {iconSize?:string; direction?:"row" | "column"}) => (
 	<nav className="Socials" aria-label="Social media links">
 		<ul style={{flexDirection: direction}}>
-			<li><a href="#"><img style={{width: iconSize}} src="/github-icon.svg" alt="Github" /></a></li>
-			<li><a href="#"><img style={{width: iconSize}} src="/linkedin-icon.svg" alt="Linkedin" /></a></li>
+			<li><a href="https://github.com/VonRiddarn" target="_blank" rel="noopener noreferrer" ><img style={{width: iconSize}} src="/github-icon.svg" alt="Github" /></a></li>
+			<li><a href="https://www.linkedin.com/in/timmyohman/" target="_blank" rel="noopener noreferrer"><img style={{width: iconSize}} src="/linkedin-icon.svg" alt="Linkedin" /></a></li>
 		</ul>
 	</nav>
 );
