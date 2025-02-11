fead4ad5d7fafaeb13393e24a319372119f16bf1
VonRiddarn <vonriddarn@gmail.com>
2025-02-11 16:23:41 +0100

    feat: added CartegoryRow component

---
+++ src/components/CategoryRow/CategoryRow.tsx
@@ -0,0 +1,17 @@
+import "./CategoryRow.scss";
+
+const CategoryRow = () => (
+	<nav className="CategoryRow">
+		<ul>
+			<li>Igneous</li>
+			<li>Sedimentary</li>
+			<li>Metamorphic</li>
+			<li>Granite</li>
+			<li>Limestone</li>
+			<li>Marble</li>
+			<li>Special</li>
+		</ul>
+	</nav>
+)
+
+export default CategoryRow;
