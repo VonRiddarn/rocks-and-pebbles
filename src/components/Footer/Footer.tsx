73d3065aa49d310786eb4a7fb4a3519641b0a09d
VonRiddarn <vonriddarn@gmail.com>
2025-02-11 21:45:14 +0100

    feat: created a Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import "./Footer.scss";
+
+const Footer = () => (
+	<footer className="Footer">
+		<p>2025 Timmy Öhman</p>
+		<p>MIT License, use as you wish ♥</p>
+		<ul>
+			<li><a href="#">Github</a></li>
+			<li><a href="#">Linkedin</a></li>
+		</ul>
+	</footer>
+);
+
+export default Footer;
