4bc45c94d51a30282fde64cfba674bc3209eca0d
VonRiddarn <vonriddarn@gmail.com>
2025-02-11 16:28:58 +0100

    feat: changed default color from silver to light blue

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,4 +1,4 @@
-const Divider = ({ height = "3px", color = "silver", width = "100%" }: { height?: string; color?: string; width?: string}) => (
+const Divider = ({ height = "3px", color = "#0e66da", width = "100%" }: { height?: string; color?: string; width?: string}) => (
 
 	<div style = {{display: "block", height: height, backgroundColor: color, width: width}}></div>
 );
