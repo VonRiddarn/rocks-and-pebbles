1cf90ac28a5f537d80f8eea912e2f27a2975a108
VonRiddarn <vonriddarn@gmail.com>
2025-02-11 16:37:09 +0100

    feat: moved search buttons aspect ratio into tsx file for better integration

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,15 +1,21 @@
 import "./Searchbar.scss";
 
-const Searchbar = ({height = "48px", width="42%"}: {height?:string; width?:string}) => {
+const Searchbar = ({height = "48px", width="42%", aspect="1.25/1"}: {height?:string; width?:string; aspect?:string}) => {
 	
 	const buttonHeight = `calc(${height} * 0.8)`;
 	const buttonIndent = `calc(${height} * 0.1)`;
+	const aspectW = aspect.split("/")[0].trim();
 	
 	return (
 		<div className="Searchbar" style={{height: height, width: width}}>
 			<form style={{borderRadius: height, paddingRight: `calc(${height} * 0.1)`}}>
-				<input type="text" style={{width: `calc(100% - ${height})`}} placeholder="Search..." />
-				<button style={{height: buttonHeight, borderTopRightRadius: buttonHeight, borderBottomRightRadius: buttonHeight, right: buttonIndent}}>
+				<input type="text" style={{width: `calc(100% - calc(${height} * ${aspectW}))`}} placeholder="Search..." />
+				<button style={{
+					height: buttonHeight, 
+					borderTopRightRadius: buttonHeight, 
+					borderBottomRightRadius: buttonHeight, 
+					right: buttonIndent,
+					aspectRatio: aspect}}>
 					<img src="search.svg" alt="SEARCH" />
 				</button>
 			</form>
