16b4a5e68353ae96d22f3e6cd3c0c25448d04563
VonRiddarn <vonriddarn@gmail.com>
2025-02-11 22:19:51 +0100

    feat: added Socials component to Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,11 @@
+import Socials from "../Socials/Socials";
 import "./Footer.scss";
 
 const Footer = () => (
 	<footer className="Footer">
 		<p>2025 Timmy Öhman</p>
 		<p>MIT License, use as you wish ♥</p>
-		<ul>
-			<li><a href="#">Github</a></li>
-			<li><a href="#">Linkedin</a></li>
-		</ul>
+		<Socials />
 	</footer>
 );
 
