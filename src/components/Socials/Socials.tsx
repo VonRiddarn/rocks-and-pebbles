52e5b2da8f0285a7cf4eec0976fbe9c91d62f205
VonRiddarn <vonriddarn@gmail.com>
2025-02-11 22:18:57 +0100

    feat: added Socials component

---
+++ src/components/Socials/Socials.tsx
@@ -0,0 +1,12 @@
+import "./Socials.scss";
+
+const Socials = ({iconSize = "48px", direction = "row"}: {iconSize?:string; direction?:"row" | "column"}) => (
+	<nav className="Socials" aria-label="Social media links">
+		<ul style={{flexDirection: direction}}>
+			<li><a href="#"><img style={{maxWidth: iconSize}} src="/github-icon.svg" alt="Github" /></a></li>
+			<li><a href="#"><img style={{maxWidth: iconSize}} src="/linkedin-icon.svg" alt="Linkedin" /></a></li>
+		</ul>
+	</nav>
+);
+
+export default Socials;
