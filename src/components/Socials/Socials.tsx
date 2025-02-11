d21400ddf5ab1084d9c1810259cd6bb8f01ebf0d
VonRiddarn <vonriddarn@gmail.com>
2025-02-11 22:29:02 +0100

    feat: changed how the iconsize works on Scoials AND re-added the linkedin SVG

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -3,8 +3,8 @@ import "./Socials.scss";
 const Socials = ({iconSize = "48px", direction = "row"}: {iconSize?:string; direction?:"row" | "column"}) => (
 	<nav className="Socials" aria-label="Social media links">
 		<ul style={{flexDirection: direction}}>
-			<li><a href="#"><img style={{maxWidth: iconSize}} src="/github-icon.svg" alt="Github" /></a></li>
-			<li><a href="#"><img style={{maxWidth: iconSize}} src="/linkedin-icon.svg" alt="Linkedin" /></a></li>
+			<li><a href="#"><img style={{width: iconSize}} src="/github-icon.svg" alt="Github" /></a></li>
+			<li><a href="#"><img style={{width: iconSize}} src="/linkedin-icon.svg" alt="Linkedin" /></a></li>
 		</ul>
 	</nav>
 );
