cbd8660fda7885b08b8992442b1f2f51317126d9
VonRiddarn <vonriddarn@gmail.com>
2025-02-11 11:30:23 +0100

    feat: added Divider to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,17 @@
 import Cart from "../Cart/Cart";
+import Divider from "../Divider/Divider";
 import "./Header.scss";
 
 const Header = () => (
 	<header>
-		<span>
-			<img src="icon.svg" alt="ICON" />
-			<h1>Rocks & Pebbles</h1>
-		</span>
-		<Cart />
+		<section>
+			<span>
+				<img src="icon.svg" alt="ICON" />
+				<h1>Rocks & Pebbles</h1>
+			</span>
+			<Cart />
+		</section>
+		<Divider />
 	</header>
 );
 
