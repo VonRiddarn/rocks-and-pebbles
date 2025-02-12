bc0d1d7f1d81fd2bfb3472952ef2552e8bde248a
VonRiddarn <vonriddarn@gmail.com>
2025-02-12 09:54:27 +0100

    feat: refactored all components to be correctly typed

--- src/components/ui/Divider/Divider.tsx
+++ src/components/ui/Divider/Divider.tsx
@@ -1,4 +1,10 @@
-const Divider = ({ height = "3px", color = "#0e66da", width = "100%" }: { height?: string; color?: string; width?: string}) => (
+type DividerProps = { 
+	height?: string; 
+	color?: string; 
+	width?: string
+}
+
+const Divider = ({ height = "3px", color = "#0e66da", width = "100%" }: DividerProps) => (
 
 	<div style = {{display: "block", height: height, backgroundColor: color, width: width}}></div>
 );
--- src/components/ui/Socials/Socials.tsx
+++ src/components/ui/Socials/Socials.tsx
@@ -1,10 +1,23 @@
 import "./Socials.scss";
 
-const Socials = ({iconSize = "48px", direction = "row"}: {iconSize?:string; direction?:"row" | "column"}) => (
+type SocialsProps = {
+	iconSize?:string; 
+	direction?:"row" | "column"
+}
+
+const Socials = ({iconSize = "48px", direction = "row"}:SocialsProps) => (
 	<nav className="Socials" aria-label="Social media links">
 		<ul style={{flexDirection: direction}}>
-			<li><a href="https://github.com/VonRiddarn" target="_blank" rel="noopener noreferrer" ><img style={{width: iconSize}} src="/src/assets/github-icon.svg" alt="Github" /></a></li>
-			<li><a href="https://www.linkedin.com/in/timmyohman/" target="_blank" rel="noopener noreferrer"><img style={{width: iconSize}} src="/src/assets/linkedin-icon.svg" alt="Linkedin" /></a></li>
+			<li>
+				<a href="https://github.com/VonRiddarn" target="_blank" rel="noopener noreferrer" >
+					<img style={{width: iconSize}} src="/src/assets/github-icon.svg" alt="Github" />
+				</a>
+			</li>
+			<li>
+				<a href="https://www.linkedin.com/in/timmyohman/" target="_blank" rel="noopener noreferrer">
+					<img style={{width: iconSize}} src="/src/assets/linkedin-icon.svg" alt="Linkedin" />
+				</a>
+			</li>
 		</ul>
 	</nav>
 );
