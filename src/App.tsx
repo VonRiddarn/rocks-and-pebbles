da6e473795cce48909a9aab89212ef4b75bb77cd
VonRiddarn <vonriddarn@gmail.com>
2025-02-13 13:27:47 +0100

    feat: created Main component AND made the app make use of it instead of hard coded main element

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import "./App.scss";
 import Footer from "./components/layout/Footer/Footer";
 import Header from "./components/layout/Header/Header";
+import Main from "./components/layout/Main/Main";
 
 function App() {
 	return (
 		<>
 			<Header />
-			<main style={{ height: "calc(100vh - 281px)" }}>
-				<p>MAIN</p>
-			</main>
+			<Main />
 			<Footer />
 		</>
 	);
---
+++ src/components/layout/Main/Main.tsx
@@ -0,0 +1,11 @@
+import "./Main.scss";
+
+const Main = () => {
+	return (
+		<main className="Main">
+			<div>StoreContainer</div>
+		</main>
+	);
+};
+
+export default Main;
