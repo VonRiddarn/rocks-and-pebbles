3bd0a6ad70808d1af86f66c6e452bb84853d6fb7
VonRiddarn <vonriddarn@gmail.com>
2025-02-13 15:07:24 +0100

    feat: added state variable to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import React from "react";
 import "./App.scss";
 import Footer from "./components/layout/Footer/Footer";
 import Header from "./components/layout/Header/Header";
 import Main from "./components/layout/Main/Main";
 
 function App() {
+	const [filterText, setFilterText] = React.useState("");
+
 	return (
 		<>
 			<Header />
